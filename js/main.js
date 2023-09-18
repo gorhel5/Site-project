caa92dda82ef4af3c2edb745abc0845732e75d29
gorhel5 <Yaroslavlit@gmail.com>
2023-09-18 19:24:54 +0300

    nav

--- js/main.js
+++ js/main.js
@@ -3,29 +3,38 @@ const header = document.querySelector('.header');
 const gamesBlock = document.querySelector('.games-block');
 const body = document.querySelector('body');
 const navListGamesEl = document.querySelector('.nav-list--games');
+const navListItems = document.querySelectorAll('.nav-list__item');
 const navButton = document.querySelector('.nav-button');
 const navButtonImg = document.querySelector('.nav-button__image');
 
-function createMenuElement (text, list) {
-    navListGamesEl.remove();
+function createMenuElement (text) {
 
     const li = document.createElement('li');
     const a = document.createElement('a');
 
-    li.className = 'nav-list__item jj';
-    a.className = 'nav-list__link vv';
+    li.className = `nav-list__item jj`;
+    a.className = 'nav-list__link ';
 
     li.append(a);
-    list.append(li);
+    navList.append(li);
     a.innerText = `${text}`;
 }
 function deleteMenuElements () {
-    document.querySelectorAll('.jj').forEach(el => {
+
+    document.querySelectorAll(`.jj`).forEach(el => {
         el.remove()
-    })
-    document.querySelectorAll('.vv').forEach(el => {
-        el.remove()
-    })
+    });
+
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+
+    li.className = 'nav-list__item nav-list__item--games';
+    a.className = 'nav-list__link';
+
+    li.append(a);
+    navList.append(li);
+    a.innerText = `Games`;
+
 }
 
 navButton.addEventListener('click', () => {
@@ -34,18 +43,14 @@ navButton.addEventListener('click', () => {
         gamesBlock.style.display = 'none';
         body.style.overflow = 'hidden';
 
-        createMenuElement('Sprint', navList);
-        createMenuElement('Audio-call', navList);
+        createMenuElement('Sprint');
+        createMenuElement('Audio-call');
+        document.querySelector('.nav-list__item--games').remove();
     } else {
         navButtonImg.src = './img/icons/Burger.svg';
         body.style.overflow = '';
         gamesBlock.style.display = 'flex';
 
         deleteMenuElements();
-        createMenuElement('Games', navList);
     }
-})
-
-
-
-
+});
